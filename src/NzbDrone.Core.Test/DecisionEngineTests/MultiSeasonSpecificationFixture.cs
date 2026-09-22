95e8a5e4510c8f76b7ce5540e8ed72955b67fe5b
nitrouscars-cloud <nitrouscars@gmail.com>
2026-09-22 00:14:35 -0400

    Fix multi-season test namespace import

--- src/NzbDrone.Core.Test/DecisionEngineTests/MultiSeasonSpecificationFixture.cs
+++ src/NzbDrone.Core.Test/DecisionEngineTests/MultiSeasonSpecificationFixture.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using FizzWare.NBuilder;
 using FluentAssertions;
 using NUnit.Framework;
+using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
